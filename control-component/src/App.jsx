c4639b4313d9cbc4a2d75d395d8eb8f4379d4ed5
Krisha0112 <krishamangukiya0112@gmail.com>
2025-04-30 02:13:10 -0700

    30/4/2024

--- control-component/src/App.jsx
+++ control-component/src/App.jsx
@@ -1,30 +1,64 @@
+import { useState } from "react"
+
 function App() {
 
+  const [name, setName] = useState("");
+  const [phone , setPhone] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let obj = {
+      id : Math.floor(Math.random()*10000),
+      name : name,
+      phone: phone
+    }
+
+    console.log("Submitted object:", obj);
+
+    setName("");
+    setPhone("");
+  }
+
   return (
-    <>
-      <div align="center">
-        <h1>Control Component</h1>
-        <form>
-          <table border={1}>
-            <tbody> 
-              <tr>
-                <td>Name </td>
-                <td><input type="text" /></td>
-              </tr>
-              <tr>
-                <td>Phone</td>
-                <td><input type="number" /></td>
-              </tr>
-              <tr>
-                <td></td>
-                <td><input type="submit" /></td>
-              </tr>
-            </tbody>
-          </table>
-        </form>
-      </div>
-    </>
+    <div align="center">
+      <h1>Control Component</h1>
+      <form onSubmit={handleSubmit}>
+        <table border={1}>
+          <tbody> 
+            <tr>
+              <td>Name</td>
+              <td>
+                <input
+                  type="text"
+                  onChange={(e) => setName(e.target.value)}
+                  value={name}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>Phone</td>
+              <td>
+                <input
+                  type="number"
+                  onChange={(e) => setPhone(e.target.value)}
+                  value={phone}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>
+                <input type="submit" />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </form>
+    </div>
   )
 }
 
-export default App
+export default App;
+
+  
