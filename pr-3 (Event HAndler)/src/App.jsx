f9039566c64d3856de746b9f5ead7a06250f2416
Krisha0112 <krishamangukiya0112@gmail.com>
2025-05-09 22:26:17 -0700

    10/5/1025

---
+++ pr-3 (Event HAndler)/src/App.jsx
@@ -0,0 +1,27 @@
+
+import { useState } from "react";
+import { categories, items } from "./data"
+import Filter from "./Filter";
+
+
+function App() {
+
+  const [filterItem, setFilterItem] = useState([])
+
+  const filterData = (category) => {
+    const filter = items.filter(items => items.category == category)
+    setFilterItem(filter);
+
+  }
+
+  return (
+
+    <Filter
+      categories={categories}
+      items={items}
+      filterItem={filterItem}
+      filterData={filterData} />
+  )
+}
+
+export default App
