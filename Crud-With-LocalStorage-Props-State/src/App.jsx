d4b22ed02c3d445e7b3145b333b2123e7d690920
KBthumar180 <kavya.thummar@gmail.com>
2025-05-22 11:23:55 +0530

    22/5/2025

---
+++ Crud-With-LocalStorage-Props-State/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+import Form from "./form/Form"
+import View from "./table/View"
+
+function App() {
+
+ 
+
+  return (
+    <>
+      <Form
+      />
+      <View />
+    </>
+  )
+}
+
+export default App
---
+++ Crud-With-LocalStorage-Props-State/src/table/View.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function View() {
+  return (
+    <div align="center">
+      <h1>View User</h1>
+      <table border={1}>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Password</th>
+            <th>Gender</th>
+            <th>Courses</th>
+            <th>City</th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  )
+}
+
+export default View
