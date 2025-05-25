629e98c24a8fe70abe13b91200eda0990da89493
Krisha0112 <krishamangukiya0112@gmail.com>
2025-05-25 01:52:32 -0700

    25/5/2025

---
+++ Pr-5 Carrot Home Page/src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
+import CardX from "./components/CardX";
+import Card2 from "./components/Card2";
+import Banner2 from "./components/Banner2";
+import Card4 from "./components/Card4";
+import Card3 from "./components/Card3";
+import News from "./components/News";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Navbar />
+      <Banner />
+      <CardX/>
+      <Card2/>
+      <Banner2/>
+      <Card4/>
+      <Card3/>
+      <News/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
+
+
+// https://preview.themeforest.net/item/carrot-multipurpose-ecommerce-html-template/full_screen_preview/50548868?_ga=2.99423454.1916608325.1747906906-930134280.1747906906
